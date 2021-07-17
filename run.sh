53f528e777c8d4b66577ebfd72a9b57641af9dbe
nathan <snockow6@protonmail.com>
2021-07-17 20:52:18 +1200

    update run.sh

--- run.sh
+++ run.sh
@@ -7,6 +7,19 @@ else
   pip3 install ansible
 fi
 
+echo "make sure external ansible modules are installed"
+# if directory doesnt exist download ansible collection posix
+if [ ! -d "~/.ansible/collections/ansible_collections/ansible/posix"] 
+then
+  ansible-galaxy collection install ansible.posix
+fi
+
+# if directory doesnt exist download ansible collection general
+if [ ! -d "~/.ansible/collections/ansible_collections/community/general/"]
+then 
+  ansible-galaxy collection install community.general
+fi 
+
 echo "Checking Connection with Server"
 if ping -c 1 bigpuffer.shed.lan &> /dev/null
 then
