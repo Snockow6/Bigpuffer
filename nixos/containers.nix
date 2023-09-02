bc44e334eba35db77f4205ccab861abc322b164e
snockow6 <nst8021@hotmail.com>
2023-09-02 18:59:59 +1200

    added Sonarr Transmission jackett Containers

--- nixos/containers.nix
+++ nixos/containers.nix
@@ -8,16 +8,16 @@
         PGID = "101009";
         TZ = "Pacific/Auckland";
       };
-      volumes = ["/Containers/Jellyfin/Config:/config:rw"];
+      volumes = [ "/Containers/Jellyfin/Config:/config:rw" ];
     };
     Pihole = {
       image = "docker.io/pihole/pihole:latest";
       ports = [ "53:53/tcp" "53:53/udp" "80:80/tcp" ];
-      volumes = [ "/Containers/Pihole/Config:/etc/pihole/:rw" "/Containers/Pihole/dnsmasqd:/etc/dnsmasq.d:rw"];
       environment = {
         DNSMASQ_USER = "root";
         TZ = "Auckland/Pacific";
       };
+      volumes = [ "/Containers/Pihole/Config:/etc/pihole/:rw" "/Containers/Pihole/dnsmasqd:/etc/dnsmasq.d:rw"];
     };
     Jackett = {
       image = "ghcr.io/linuxserver/jackett:latest";
@@ -28,6 +28,26 @@
         TZ = "Auckland/Pacific";
       };
       volumes = [ "/Containers/Jackett/Config:/config:rw" "/Containers/Jackett/Downloads:/downloads:rw" ];
-    }
+    };
+    Sonarr = {
+      image = "ghcr.io/linuxserver/sonarr:latest";
+      ports = [ "8989:8989" ];
+      environment = {
+        PUID = "101009";
+        GUID = "101009";
+        TZ = "Auckland/Pacific";
+      };
+      volumes = [ "/Containers/Sonarr/Config:/config:rw" "/Containers/Transmission/Downloads:/downloads:rw" "/Tank/Shares/Anime:/Anime" "/Tank/Shares/TV:/TV" "/Tank/Shares/Youtube:/Youtube" ];
+    };
+    Transmission = {
+      image = "ghcr.io/linuxserver/transmission:latest";
+      ports = [ "9091:9091" ];
+      environment = {
+        PUID = "101009";
+        GUID = "101009";
+        TZ = "Auckland/Pacific";        
+      };
+      volumes = [ "/Containers/Tansmission/Config:/config:rw" "/Containers/Transmission/Downloads:/downloads:rw" "/Containers/Tansmission/Watch:/watch:rw"];
+    };
   };
 }
